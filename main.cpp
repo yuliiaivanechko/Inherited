e62aa2c23d277d9d4a56258ed9cbfd4ca328aeb5
yuliiaivanechko <yuliaivanechko@gmail.com>
2021-12-01 21:31:12 +0200

    Initial

--- main.cpp
+++ main.cpp
@@ -1,127 +1,111 @@
-//
-//  main.cpp
-//  LRUCache
-//
-//  Created by Yuliia Ivanechko on 22.11.2021.
-//
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
-#include <algorithm>
-#include <set>
-#include <cassert>
+#include<iostream>
+
 using namespace std;
 
-struct Node
+class A
 {
-   Node* next;
-   Node* prev;
-   int value;
-   int key;
-   Node(Node* p, Node* n, int k, int val):prev(p),next(n),key(k),value(val){};
-   Node(int k, int val):prev(NULL),next(NULL),key(k),value(val){};
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
+
+    protected:
+        void func(int & a)
+        {
+            a = a * 2;
+            inc();
+        }
+    public:
+        int getA(){
+            return callA;
+        }
 };
 
-class Cache
+class B
 {
-   
-   protected:
-   map<int,Node*> mp; //map the key to the node in the linked list
-   int cp;  //capacity
-   Node* tail; // double linked list tail pointer
-   Node* head; // double linked list head pointer
-   virtual void set(int, int) = 0; //set function
-   virtual int get(int) = 0; //get function
-
+    public:
+        B(){
+            callB = 0;
+        }
+    private:
+        int callB;
+        void inc(){
+            callB++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 3;
+            inc();
+        }
+    public:
+        int getB(){
+            return callB;
+        }
 };
 
-class LRUCache : public Cache
+class C
 {
-private:
-    int capacity;
-    int pointer = 0;
-public:
-    LRUCache(int n)
-    {
-        this -> capacity = n;
-        this -> head = new Node (0, 0);
-
-        Node* current = head;
-        for (int i = 0; i != n; i++)
-        {
-            Node* temp = new Node (0, 0);
-            current -> next = temp;
-            temp -> prev = current;
-            mp.emplace(i, temp);
-            current = temp;
+    public:
+        C(){
+            callC = 0;
         }
-        this -> tail = current;
-    }
-    
-    virtual void set(int a, int b)
-    {
-        if (this -> pointer == this -> capacity - 1 )
-        {
-            this -> pointer = 0;
+    private:
+        int callC;
+        void inc(){
+            callC++;
         }
-        std::map<int, Node*>::iterator result = std::find_if(mp.begin(), mp.end(), [a](const std::pair<int, Node*>& el){ return el.second -> key == a; });
-        if (result == mp.end())
+    protected:
+        void func(int & a)
         {
-            this -> mp[this -> pointer] -> key = a;
-            this -> mp[this -> pointer] -> value = b;
-            this -> pointer++;
+            a = a * 5;
+            inc();
         }
-        else
-        {
-            result -> second -> value = b;
+    public:
+        int getC(){
+            return callC;
         }
-
-    }
-    virtual int get(int f)
-    {
-        std::map<int, Node*>::iterator result = std::find_if(mp.begin(), mp.end(), [f](const std::pair<int, Node*>& el){ return el.second -> key == f; });
-        if (result == mp.end())
-        {
-            return -1;
-        }
-        else
-        {
-            return result -> second -> value;
-        }
-    }
-    ~LRUCache()
-    {
-        for (std::size_t i = 0; i != this->capacity; ++i)
-        {
-            Node* temp = tail -> prev;
-            delete tail;
-            temp -> next = nullptr;
-            this -> tail = temp;
-        }
-        delete this -> head;
-        head = nullptr;
-    }
 };
 
-int main() {
-   int n, capacity, i;
-   cin >> n >> capacity;
-   LRUCache l(capacity);
+class D
+{
 
-   for(i = 0; i < n; i++) {
-      string command;
-      cin >> command;
-      if(command == "get") {
-         int key;
-         cin >> key;
-         cout << l.get(key) << endl;
-      }
-      else if(command == "set") {
-         int key, value;
-         cin >> key >> value;
-         l.set(key,value);
-      }
-   }
-   return 0;
+    int val;
+    public:
+        //Initially val is 1
+         D()
+         {
+             val = 1;
+         }
+
+
+         //Implement this function
+         void update_val(int new_val)
+         {
+
+            
+         }
+         //For Checking Purpose
+         void check(int); //Do not delete this line.
+};
+
+void D::check(int new_val)
+{
+    update_val(new_val);
+    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+}
+
+
+int main()
+{
+    D d;
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);
+
 }
